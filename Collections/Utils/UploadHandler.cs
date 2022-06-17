687f43c835c3d994b4e3ecd788011283bf8ea516
Umarbek Tulaev <umarbek@mail.uz>
2022-06-17 16:44:39 +0500

    upload handler added

---
+++ Collections/Utils/UploadHandler.cs
@@ -0,0 +1,52 @@
+﻿namespace Collections.Utils
+{
+    public class UploadHandler
+    {
+        private static IWebHostEnvironment _env;
+        private const string ImagePath = "images";
+
+        public UploadHandler(IWebHostEnvironment env)
+        {
+            _env = env;
+        }
+
+        public async Task<string> UploadAsync(IFormFile file)
+        {
+            string ext = Path.GetExtension(file.FileName);
+            string fileName = String.Concat(Path.GetRandomFileName(), ext);
+            string path = Path.Combine(_env.WebRootPath, fileName);
+
+            using (var fs = new FileStream(path, FileMode.Create, FileAccess.Write))
+            {
+                await file.CopyToAsync(fs);
+            }
+
+            return fileName;
+        }
+
+        public async Task<string> UploadAsync(IFormFile file, string existingFilePath)
+        {
+            string ext = Path.GetExtension(file.FileName);
+            string fileName = String.Concat(Path.GetRandomFileName(), ext);
+            string path = Path.Combine(_env.WebRootPath, fileName);
+
+            if (String.IsNullOrWhiteSpace(existingFilePath))
+            {
+                string oldImagePath = Path.Combine(GetImagesPath, existingFilePath);
+                if (System.IO.File.Exists(oldImagePath))
+                {
+                    System.IO.File.Delete(oldImagePath);
+                }
+            }
+
+            using (var fs = new FileStream(path, FileMode.Create, FileAccess.Write))
+            {
+                await file.CopyToAsync(fs);
+            }
+
+            return fileName;
+        }
+
+        public static string GetImagesPath => Path.Combine(_env.WebRootPath, ImagePath);
+    }
+}
